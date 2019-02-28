447f20c59374f952276864b1d7da14f5d1dfa2b6
NMoore42 <nicholas.moore5@icloud.com>
2019-02-28 15:52:24 -0600

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,79 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, value|
+    languages[style].each do |language, attribute|
+      languages[style][language].each do |type, type_value|
+        if new_hash[language]
+          new_hash[language][:style] << style
+        else
+          new_hash[language] = {type => type_value, :style => [style]}
+        end
+      end
+    end
+  end
+  new_hash
 end
+
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
+{
+  :ruby => {
+    :type => "interpreted",
+    :style => [:oo]
+  },
+  :javascript => {
+    :type => "interpreted",
+    :style => [:oo, :functional]
+  },
+  :python => {
+    :type => "interpreted",
+    :style => [:oo]
+  },
+  :java => {
+    :type => "compiled",
+    :style => [:oo]
+  },
+  :clojure => {
+    :type => "compiled",
+    :style => [:functional]
+  },
+  :erlang => {
+    :type => "compiled",
+    :style => [:functional]
+  },
+  :scala => {
+    :type => "compiled",
+    :style => [:functional]
+  }
+}
